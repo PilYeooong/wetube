061e78780ec05804e13676cfeff542286a2a9961
PilYeong <pilyeooong@gmail.com>
2020-01-09 00:05:27 +0900

    ADD: express 와 pug 를이용하여 백앤드 구축

---
+++ db.js
@@ -0,0 +1,50 @@
+export const videos = [
+    {
+        id: 123093,
+        title: 'Video awesome',
+        description: 'This is something I love',
+        views: 24,
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator: {
+            id: 123123,
+            name: "Max",
+            email: "Max@google.com"
+        }
+    },
+    {
+        id: 324393,
+        title: 'Video NICE',
+        description: 'WOWOWOWOWOWOWO',
+        views: 24,
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator: {
+            id: 123123,
+            name: "Max",
+            email: "Max@google.com"
+        }
+    }, 
+    {
+        id: 55555,
+        title: 'Video SUPER',
+        description: 'WOW',
+        views: 24,
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator: {
+            id: 123123,
+            name: "Max",
+            email: "Max@google.com"
+        }
+    },
+    {
+        id: 10923,
+        title: 'Video Fantastic',
+        description: 'This is something I love it',
+        views: 24,
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator: {
+            id: 123123,
+            name: "Max",
+            email: "Max@google.com"
+        }
+    },
+]
