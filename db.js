c59f34dad6136c9e1b2aea3263cdb0f434eb5732
PilYeong <pilyeooong@gmail.com>
2020-01-11 23:18:52 +0900

    ADD: 비디오 관련 백앤드기능 + Header designing with scss

--- db.js
+++ db.js
@@ -1,50 +1,17 @@
-export const videos = [
-    {
-        id: 123093,
-        title: 'Video awesome',
-        description: 'This is something I love',
-        views: 24,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 123123,
-            name: "Max",
-            email: "Max@google.com"
-        }
-    },
-    {
-        id: 324393,
-        title: 'Video NICE',
-        description: 'WOWOWOWOWOWOWO',
-        views: 24,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 123123,
-            name: "Max",
-            email: "Max@google.com"
-        }
-    }, 
-    {
-        id: 55555,
-        title: 'Video SUPER',
-        description: 'WOW',
-        views: 24,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 123123,
-            name: "Max",
-            email: "Max@google.com"
-        }
-    },
-    {
-        id: 10923,
-        title: 'Video Fantastic',
-        description: 'This is something I love it',
-        views: 24,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 123123,
-            name: "Max",
-            email: "Max@google.com"
-        }
-    },
-]
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config()
+
+mongoose.connect(process.env.MONGO_URL, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useFindAndModify: false
+});
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("Connected to DB")
+const handleError = error => console.log(`Error on DB Connection:${error}`);
+
+db.once("open", handleOpen);
+db.on("error", handleError);
